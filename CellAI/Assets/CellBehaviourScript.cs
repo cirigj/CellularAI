4144e48c782e3c236ecd63f7d34a8cb1c23f991e
Parshva Shah <g7parsh@gmail.com>
2014-11-20 17:46:43 -0500

    Testing things

---
+++ CellAI/Assets/CellBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CellBehaviourScript : MonoBehaviour {
+	public float intake;
+	public float processingSpeed;
+	public float sugarCap;
+	public float maxSpeed;
+	public int 	 foresight;
+	public float useEff;
+	public float energyCap;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
